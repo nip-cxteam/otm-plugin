f4f552f414fcf3c457e539095bb9eb5aa1012ca2
Gabriel Gomes <gomes@me.berkeley.edu>
2018-10-05 18:51:50 -0700

    fix test

--- src/test/java/test/TestPlugin.java
+++ src/test/java/test/TestPlugin.java
@@ -10,31 +10,32 @@ import static org.junit.Assert.fail;
 
 public class TestPlugin {
 
+    String out_requast_file = "config/output_request.xml";
+    String output_folder = "output/";
+    float start_time = 0f;
+    float duration = 3200f;
+    float sim_dt = 1f;
+
     @Test
-    public void test_run_both(){
-
+    public void run_plugin(){
         try {
-
-            String out_requast_file = "config/output_request.xml";
-            String output_folder = "output/";
-            float start_time = 0f;
-            float duration = 3200f;
-            float sim_dt = 1f;
-
-            // no plugin
-            API api_no_plugin = OTM.load("config/intersection.xml",sim_dt,true,"ctm");
-            api_no_plugin.run("no_plugin",out_requast_file,output_folder,start_time,duration);
-
-            // plugin
-            API api_plugin = OTM.load("config/intersection_plugin.xml",sim_dt,true,"ctm");
-            api_plugin.run("plugin",out_requast_file,output_folder,start_time,duration);
-
+            API api = OTM.load("config/intersection_plugin.xml",sim_dt,true,"ctm");
+            api.run("plugin",out_requast_file,output_folder,start_time,duration);
         } catch (OTMException e) {
             System.out.print(e);
             fail();
         }
     }
 
-
+    @Test
+    public void run_no_plugin(){
+        try {
+            API api_no_plugin = OTM.load("config/intersection.xml",sim_dt,true,"ctm");
+            api_no_plugin.run("no_plugin",out_requast_file,output_folder,start_time,duration);
+        } catch (OTMException e) {
+            System.out.print(e);
+            fail();
+        }
+    }
 
 }
